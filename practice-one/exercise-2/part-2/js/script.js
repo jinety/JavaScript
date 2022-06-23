566a358431e8d845b12687c15b702e54364e1ae1
Jine Ty <vhoa1000@gmail.com>
2022-06-23 15:37:11 +0700

    Update part 1

--- practice-one/exercise-2/part-2/js/script.js
+++
@@ -1,8 +0,0 @@
-const getFormvalue = () => {
-  let x=document.getElementById("form1");
-  for(let i=0;i<x.length;i++) {
-   if(x.elements[i].value!='Submit') {  
-      alert(x.elements[i].value);
-    }  
-  }
-};
--- practice-one/exercise-2/part-3/js/script.js
+++
@@ -1,9 +0,0 @@
-const setBackground = () => {
-  const paragraphs = document.querySelectorAll('p');
-  paragraphs.forEach(paragraph => {
-    paragraph.style.background = 'green';
-  })
-};
-
-const textColorChangeButton = document.querySelector('input');
-textColorChangeButton = addEventListener('click', setBackground);
