23144bfdf9f7eca071dde1857dc3582666708bbc
Jine Ty <vhoa1000@gmail.com>
2022-06-24 10:48:12 +0700

    Update get data from form by Event Listener and rename id

--- practice-one/exercise-2/part-2/js/script.js
+++ practice-one/exercise-2/part-2/js/script.js
@@ -1,8 +1,11 @@
 const getFormValue = () => {
-  let form = document.getElementById("form1");
+  let form = document.getElementById("nameSubmissionForm");
   for(let i = 0; i < form.length; i++) {
    if(form.elements[i].value!='Submit') {  
       alert(form.elements[i].value);
     }  
   }
-};
+}
+
+const formSubmit = document.querySelector('form');
+formSubmit.addEventListener('submit', getFormValue);
