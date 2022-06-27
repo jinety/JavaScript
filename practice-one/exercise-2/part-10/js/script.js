46b5816554446dd1f86e70930f5670340a6fc455
Jine Ty <vhoa1000@gmail.com>
2022-06-27 10:57:01 +0700

    Update add blank line after variable defined

--- practice-one/exercise-2/part-10/js/script.js
+++ practice-one/exercise-2/part-10/js/script.js
@@ -1,12 +1,13 @@
 const calculateVolumeSphere = () => {
   let volume;
   let radius = document.getElementById('radius').value;
+
   radius = Math.abs(radius);
   volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
   volume = volume.toFixed(4);
   document.getElementById('volume').value = volume;
   return false;  
-};
+}
 
 const submitBtn = document.getElementById('submitBtn');
 submitBtn.addEventListener('click', calculateVolumeSphere);
