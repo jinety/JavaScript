036ebd61ac159d8cea8f161d484d5bb17c4e8a88
Jine Ty <vhoa1000@gmail.com>
2022-06-27 09:38:58 +0700

    Implement JS to count and display dropdown list items

---
+++ practice-one/exercise-2/part-9/js/script.js
@@ -0,0 +1,13 @@
+const getOptions = () => {
+  let colorSelection = document.getElementById('mySelect');
+  let txt = 'No. of items in dropdown is: ';
+  let list = document.getElementById('mySelect').length;
+  txt = txt + list;
+  for(let i = 0; i < colorSelection.length; i++) {
+    txt = txt + '\n' + colorSelection.options[i].text;
+  }
+  alert(txt);
+}
+
+const countAndDisplayItemsBtn = document.getElementById('countAndDisplayItemsBtn');
+countAndDisplayItemsBtn.addEventListener('click', getOptions);
