717c5718699f7a6483d609e986397db9627d0510
Jine Ty <vhoa1000@gmail.com>
2022-06-27 10:39:58 +0700

    Change the variable name to listLength and rename the id for btn

--- practice-one/exercise-2/part-9/js/script.js
+++ practice-one/exercise-2/part-9/js/script.js
@@ -1,14 +1,14 @@
 const getOptions = () => {
   const colorSelection = document.getElementById('colorList');
   txt = 'No. of items in dropdown is: ';
-  let colorList = document.getElementById('colorList').length;
-  txt = txt + colorList;
+  const listLength = document.getElementById('colorList').length;
+
+  txt = txt + listLength;
   for(let i = 0; i < colorSelection.length; i++) {
-    txt = `${txt}
-    ${colorSelection.options[i].text}`;
+    txt = `${txt} ${colorSelection.options[i].text}`;
   }
   alert(txt);
 }
 
-const countAndDisplayBtn = document.getElementById('countAndDisplayBtn');
-countAndDisplayBtn.addEventListener('click', getOptions);
+const showButton = document.getElementById('showBtn');
+showButton.addEventListener('click', getOptions);
