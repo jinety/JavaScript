e5388f18c07111b71b4100539bf223a013761f9c
Jine Ty <vhoa1000@gmail.com>
2022-06-28 08:36:27 +0700

    Update more notification if user entered wrong

--- practice-one/exercise-2/part-6/js/script.js
+++ practice-one/exercise-2/part-6/js/script.js
@@ -4,9 +4,17 @@ const changeTable = () => {
     let targetRow  = document.getElementById('rowNumber').value;
     let targetCol   = document.getElementById('columnNumber').value;
 
-    let targetRowCells  = rows[targetRow].cells;
-
-    targetRowCells[targetCol].innerHTML = document.getElementById('cellContent').value;
+    let text;
+    if(targetRow >= 0 && targetRow <= 2 && targetCol >= 0 && targetCol <= 1) {
+        let targetRowCells = rows[targetRow].cells;
+        targetRowCells[targetCol].innerHTML = document.getElementById('cellContent').value;
+        text = 'Successful';
+        
+    } else {
+        text = 'Failure!!! Please re-enter';
+    }
+   
+    document.getElementById("notification").innerHTML = text;
 }
 
 const changeContentBtn = document.getElementById('changeContentBtn');
