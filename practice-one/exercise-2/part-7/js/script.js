14bd49fa24911c367791039ec0e1bb2b23a5aa15
Jine Ty <vhoa1000@gmail.com>
2022-06-28 15:44:27 +0700

    Update reset button id and variable name for row and column query

--- practice-one/exercise-2/part-7/js/script.js
+++ practice-one/exercise-2/part-7/js/script.js
@@ -1,10 +1,10 @@
 const createTable = () => {
-    const inprow = document.getElementById('tableRow').value;
-    const inpcol = document.getElementById('tableCol').value;
+    const targetRow = document.getElementById('tableRow').value;
+    const targetCol = document.getElementById('tableCol').value;
 
-    for (let row = 0; row < parseInt(inprow, 10); row++) {
+    for (let row = 0; row < parseInt(targetRow, 10); row++) {
         const table = document.getElementById('table').insertRow(row);
-        for (let column = 0; column < parseInt(inpcol, 10); column++) {
+        for (let column = 0; column < parseInt(targetCol, 10); column++) {
             const tableDisplay = table.insertCell(column);
             tableDisplay.innerHTML=`Row ${row} Column ${column}`; 
         }
