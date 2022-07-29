9e4936a426f4a93b5d6cbb6d1e3edf5c400d9b14
Jine Ty <vhoa1000@gmail.com>
2022-07-29 13:41:11 +0700

    Implement the code to display the list of users to the table after an update

--- practice-three/src/js/script.js
+++ practice-three/src/js/script.js
@@ -160,6 +160,7 @@ const isValidForm = () => {
   fetch(userApi, options)
     // Parses JSON response into native JavaScript objects 
     .then((response) => response.json())
+    .then(userTableBody.insertRow(renderUserTable))
     // Show error message when API call is wrong
     .catch((error) => alert('An error occurred while creating user', error));
 }
@@ -195,8 +196,8 @@ const renderUserTable = () => {
             <td>${user.salary}</td>
             <td>${user.city}</td>
             <td class="td-btn">
-              <button type="button" class="delete-button" data-id=${user.id}>Delete</button>
-              <button type="button" class="update-button" data-id=${user.id}>Update</button>
+              <button type="button" class="btn-table delete-button" data-id=${user.id}>Delete</button>
+              <button type="button" class="btn-table update-button" data-id=${user.id}>Update</button>
             </td>
           </tr>
         `;
@@ -218,6 +219,7 @@ const renderUserTable = () => {
           getUser(item);
           submitBtn.classList.add('hide');
           updateBtnForm.classList.remove('hide');
+          cancelBtn.classList.remove('hide');
         })
       })
 
@@ -240,7 +242,7 @@ const renderUserTable = () => {
           fetch(userApi + '/' + registrationForm.getAttribute('data-id'), options)
             // Parses JSON response into native JavaScript objects 
             .then((response) => response.json())
-            .then()
+            .then(renderUserTable())
             // Show error message when API call is wrong
             .catch((error) => alert('An error occurred while update user', error));
         }
@@ -263,7 +265,7 @@ const deleteUser = (item) => {
   const userId = item.dataset.id;
 
   fetch(userApi + '/' + userId, options)
-    .then()
+    .then(item.parentElement.parentElement.remove())
     .catch((error) => alert('An error occurred while removing user', error));
 }
 
@@ -293,12 +295,14 @@ const getUser = (item) => {
 const submitForm = () => {
   if (isValidForm()) {
     handleCreateForm();
+    renderUserTable()
   }
 }
 
 cancelBtn.addEventListener('click', () => {
   submitBtn.classList.remove('hide');
   updateBtnForm.classList.add('hide');
+  cancelBtn.classList.add('hide');
 })
 
 submitBtn.addEventListener('click', submitForm);
