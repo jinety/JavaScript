ad8ab05df79e2d213fdf3fbff91ae3cd62bc43a9
Jine Ty <vhoa1000@gmail.com>
2022-09-06 09:13:04 +0700

    Example code implementation for async/await and class

---
+++ src/js/script.js
@@ -0,0 +1,46 @@
+//=========================== ASYNC/AWAIT ==================================
+const getRequest = async (url) => {
+  const res = await fetch(url);
+  if (res.ok) { 
+      return res.json();
+  } else {
+      throw new Error("Bad response");
+  }
+}
+
+const getData = async (url) => {
+  try {
+      const data = await getRequest(url);
+      console.log(data);
+  } catch(e) {
+      console.log(e);
+  }
+}
+
+getData('http://localhost:3000/posts');
+
+//============================= CLASS ====================================
+ 
+class Car {
+  constructor() {
+    this.name = 'Toyota';
+    this.color = 'Red';
+  }
+
+  showMsg() {
+    console.log(`My car ${this.name} is ${this.color}`)
+  } 
+}
+
+// Inheritance
+class newCar extends Car {
+  // Override
+  showMsg() {
+    console.log(`Car ${this.name} it's my favorite car`);
+  }
+}
+
+let myCar = new newCar();
+
+// Pack
+myCar.showMsg(); 
