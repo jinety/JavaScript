c2d75103ef13916fa26b4d3c5bbdf45c412406a3
Jine Ty <vhoa1000@gmail.com>
2022-09-22 15:03:11 +0700

    Update code for function getAccount

--- big-practice/src/js/pages/login.page.js
+++ big-practice/src/js/pages/login.page.js
@@ -1,6 +1,7 @@
 import { formValidate } from '../validates/form.validate';
 import { DocumentHelper } from '../helpers/document.helper';
 import { AccountService } from '../service/account.service';
+import { MESSAGES, EMPTY_TEXT } from '../constants/message.constant';
 import { DASHBOARD_PAGE, USERNAME_KEY } from '../constants/app.constant';
 
 class Login {
@@ -35,7 +36,45 @@ class Login {
       return;
     }
 
-    await AccountService.getAccount(data);
+    try {
+      const userList = await AccountService.getAccount(data);
+
+      // IncorrectLoginAccount
+      if (userList.length === 0) {
+        DocumentHelper.showErrorMessage(
+          this.generalWarnMsg,
+          MESSAGES.incorrectLoginAccount,
+        );
+        DocumentHelper.showErrorMessage(this.emailInput, EMPTY_TEXT);
+        DocumentHelper.showErrorMessage(this.passwordInput, EMPTY_TEXT);
+
+        return;
+      }
+
+      // Non-admin account
+      if (!userList[0].isAdmin) {
+        DocumentHelper.showErrorMessage(
+          this.generalWarnMsg,
+          MESSAGES.notAdminAccount,
+        );
+        DocumentHelper.showErrorMessage(this.emailInput, EMPTY_TEXT);
+        DocumentHelper.showErrorMessage(this.passwordInput, EMPTY_TEXT);
+
+        return;
+      }
+
+      DocumentHelper.showErrorMessage(this.emailInput, EMPTY_TEXT);
+      DocumentHelper.showErrorMessage(this.passwordInput, EMPTY_TEXT);
+      DocumentHelper.showErrorMessage(this.generalWarnMsg, EMPTY_TEXT);
+
+      // Save username to localStorage
+      localStorage.setItem(USERNAME_KEY, userList[0].email);
+
+      // Switch to dashboard page
+      window.location.href = DASHBOARD_PAGE;
+    } catch {
+      DocumentHelper.showErrorMessage(this.generalWarnMsg, MESSAGES.getAccountErr);
+    }
   }
 
   /**
--- big-practice/src/js/service/account.service.js
+++ big-practice/src/js/service/account.service.js
@@ -1,54 +1,11 @@
 import { ACCOUNTS_API } from '../constants/url-api.constant';
-import { MESSAGES, EMPTY_TEXT } from '../constants/message.constant';
-import { DASHBOARD_PAGE, USERNAME_KEY } from '../constants/app.constant';
 import { apiService } from './api.service';
-import { DocumentHelper } from '../helpers/document.helper';
 
 export class AccountService {
   static async getAccount(data) {
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-    const generalWarnMsg = document.querySelector('.general-warn-msg');
     const url = `${ACCOUNTS_API}?email=${data.email}&password=${data.password}`;
+    const account = await apiService.get(url);
 
-    try {
-      const userList = await apiService.get(url);
-
-      // IncorrectLoginAccount
-      if (userList.length === 0) {
-        DocumentHelper.showErrorMessage(
-          generalWarnMsg,
-          MESSAGES.incorrectLoginAccount,
-        );
-        DocumentHelper.showErrorMessage(emailInput, EMPTY_TEXT);
-        DocumentHelper.showErrorMessage(passwordInput, EMPTY_TEXT);
-
-        return;
-      }
-
-      // Non-admin account
-      if (!userList[0].isAdmin) {
-        DocumentHelper.showErrorMessage(
-          generalWarnMsg,
-          MESSAGES.notAdminAccount,
-        );
-        DocumentHelper.showErrorMessage(emailInput, EMPTY_TEXT);
-        DocumentHelper.showErrorMessage(passwordInput, EMPTY_TEXT);
-
-        return;
-      }
-
-      DocumentHelper.showErrorMessage(emailInput, EMPTY_TEXT);
-      DocumentHelper.showErrorMessage(passwordInput, EMPTY_TEXT);
-      DocumentHelper.showErrorMessage(generalWarnMsg, EMPTY_TEXT);
-
-      // Save username to localStorage
-      localStorage.setItem(USERNAME_KEY, userList[0].email);
-
-      // Switch to dashboard page
-      window.location.href = DASHBOARD_PAGE;
-    } catch {
-      DocumentHelper.showErrorMessage(generalWarnMsg, MESSAGES.getAccountErr);
-    }
+    return account;
   }
 }
