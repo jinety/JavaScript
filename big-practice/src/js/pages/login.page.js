808a8446da98c95ea217a0944f07168e0fa80e49
Jine Ty <vhoa1000@gmail.com>
2022-09-22 14:23:44 +0700

    Update account verification code by getting data from API into file account.service

--- big-practice/src/js/pages/login.page.js
+++ big-practice/src/js/pages/login.page.js
@@ -1,9 +1,7 @@
 import { formValidate } from '../validates/form.validate';
-import { apiService } from '../service/api.service';
 import { DocumentHelper } from '../helpers/document.helper';
-import { ACCOUNTS_API } from '../constants/url-api.constant';
-import { MESSAGES, EMPTY_TEXT } from '../constants/message.constant';
-import { DASHBOARD_PAGE, USERNAME_KEY, LOGIN_PAGE } from '../constants/app.constant';
+import { AccountService } from '../service/account.service';
+import { DASHBOARD_PAGE, USERNAME_KEY } from '../constants/app.constant';
 
 class Login {
   loginBtn = document.getElementById('loginBtn');
@@ -29,7 +27,6 @@ class Login {
       password: ['empty'],
     };
     const validate = formValidate.validateForm(data, config);
-    const url = `${ACCOUNTS_API}?email=${data.email}&password=${data.password}`;
 
     if (!validate.isValid) {
       DocumentHelper.showErrorMessage(this.emailInput, validate.errors.email);
@@ -38,45 +35,7 @@ class Login {
       return;
     }
 
-    try {
-      const userList = await apiService.get(url);
-
-      // IncorrectLoginAccount
-      if (userList.length === 0) {
-        DocumentHelper.showErrorMessage(
-          this.generalWarnMsg,
-          MESSAGES.incorrectLoginAccount,
-        );
-        DocumentHelper.showErrorMessage(this.emailInput, EMPTY_TEXT);
-        DocumentHelper.showErrorMessage(this.passwordInput, EMPTY_TEXT);
-
-        return;
-      }
-
-      // Non-admin account
-      if (!userList[0].isAdmin) {
-        DocumentHelper.showErrorMessage(
-          this.generalWarnMsg,
-          MESSAGES.notAdminAccount,
-        );
-        DocumentHelper.showErrorMessage(this.emailInput, EMPTY_TEXT);
-        DocumentHelper.showErrorMessage(this.passwordInput, EMPTY_TEXT);
-
-        return;
-      }
-
-      DocumentHelper.showErrorMessage(this.emailInput, EMPTY_TEXT);
-      DocumentHelper.showErrorMessage(this.passwordInput, EMPTY_TEXT);
-      DocumentHelper.showErrorMessage(this.generalWarnMsg, EMPTY_TEXT);
-
-      // Save username to localStorage
-      localStorage.setItem(USERNAME_KEY, userList[0].email);
-
-      // Switch to dashboard page
-      window.location.href = DASHBOARD_PAGE;
-    } catch {
-      DocumentHelper.showErrorMessage(this.generalWarnMsg, MESSAGES.getAccountErr);
-    }
+    await AccountService.getAccount(data);
   }
 
   /**
