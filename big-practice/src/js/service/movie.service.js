5dddbb0f849b5a8605960147d7ec9b179e42e549
Jine Ty <vhoa1000@gmail.com>
2022-09-23 11:45:49 +0700

    Add comment

--- big-practice/src/js/service/movie.service.js
+++ big-practice/src/js/service/movie.service.js
@@ -12,16 +12,27 @@ export class MovieService {
   /**
    * Get movie by name in database
    *
-   * @param {string} name
+   * @param {string} name - Movie name
    */
   static async getByName(name) {
     return await apiService.get(`${MOVIES_API}?name=${name}`);
   }
 
+  /**
+   * Add new movies in database
+   *
+   * @param {string} data - The object contains the information of the movie
+   */
   static async post(data) {
     return await apiService.post(MOVIES_API, data);
   }
 
+  /**
+   * Update movie in database
+   *
+   * @param {*} id - Id of the movie in the database
+   * @param {*} data - The object contains the information of the movie
+   */
   static async putById(id, data) {
     return apiService.put(`${MOVIES_API}/${id}`, data);
   }
